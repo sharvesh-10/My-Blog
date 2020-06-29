0fd89acf067d5b56b087c535dc6eab465472f250
sharvesh-10 <sharveshsofficial@gmail.com>
2020-06-29 12:52:23 +0530

    Added all code for blog setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
-    `gatsby-pugin-styled-components`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
